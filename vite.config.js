d3977815cda566bc0bc1b43b65e7725ce50f43b6
Adam Hunter <90648351+adam-hunter13@users.noreply.github.com>
2024-12-04 16:45:06 -0700

    Edits to vite.config.js for routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// Adding below to try and get routes working
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true, // This ensures routing works locally too.
+  },
+});
